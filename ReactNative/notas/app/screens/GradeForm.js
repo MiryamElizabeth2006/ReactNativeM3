a9d295cde9c1ae9635565d103c41bb44f4a8dead
MiryamElizabeth2006 <miryamelizabeth367@gmail.com>
2024-05-26 22:39:42 -0500

    Reto 30 video adjunto en el ejercicio notas

---
+++ ReactNative/notas/app/screens/GradeForm.js
@@ -0,0 +1,97 @@
+import {View, StyleSheet, Text} from 'react-native'
+import {Button, Input} from '@rneui/base'
+import { useState } from 'react'
+import { saveGrade, updateGrade} from '../services/GradesServices'
+
+export const GradeForm=({navigation, route})=>{
+    let isNew=true;
+    let subjectR;
+    let gradeR;
+    if(route.params.notita!=null){
+        isNew=false;
+    }
+
+    if(!isNew){
+        subjectR=route.params.notita.subject;
+        gradeR=route.params.notita.grade;
+    }
+
+    const [subject, setSubject]=useState(subjectR);
+    const [grade, setGrade]=useState(gradeR==null?null:gradeR+"");
+    const [errorSubject, setErrorSubject]=useState();
+    const [errorGrade, setErrorGrade]=useState();
+    const hasError=false;
+
+    const save=()=>{
+        setErrorGrade(null);
+        setErrorSubject(null);
+        validate();
+        if(!hasError){
+            if(isNew){
+                saveGrade({subject:subject,grade:grade});
+            }else{
+                updateGrade({subject:subject,grade:grade});
+            }
+            
+            navigation.navigate('ListGradesNav');
+            route.params.fnRefresh();
+        }
+    }
+
+    const validate=()=>{
+        if(subject == null || subject == ""){
+            setErrorSubject('Debe ingresar una materia');
+            hasError=true;
+        }
+
+        let gradeFloat=parseFloat(grade);
+
+        if(gradeFloat == null || isNaN(gradeFloat) || gradeFloat<0 || gradeFloat>10) {
+            setErrorGrade('Debe ingresar una nota entre 0-10');
+            hasError=true;
+        }
+    }
+
+    return <View style={styles.container}>
+        <Input
+            value={subject}
+            onChangeText={setSubject}
+            placeholder="Ejemplo; Matematicas"
+            label="Materia"
+            errorMessage={errorSubject}
+            disabled={!isNew}
+        />
+         <Input
+            value={grade}
+            onChangeText={setGrade}
+            placeholder="0-10"
+            label="Nota"
+            keyboardType='numeric'
+            errorMessage={errorGrade}
+        />
+
+        <Button
+            title="GUARDAR"
+            icon={{
+                name: 'save',
+                type:  'entypo',
+                color: 'white'
+
+            }}
+            buttonStyle={styles.saveButton}
+            onPress={save}
+        />
+    </View>
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    saveButton:{
+        backgroundColor: 'green'
+    }
+  });
